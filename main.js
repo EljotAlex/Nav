aed01e1bbe601127f6e81d3a1cfbe49f9755c2e3
Katarzyna Kołakowska <eljot@yahoo.com>
2016-01-17 17:53:59 +0100

    Dev

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Kate on 17.01.2016.
+ */
+
+
+var li = document.createElement('button');
+var ul = document.getElementsByTagName('ul')[0];
+li.innerText = 'Developer';
+ul.appendChild(li);
